69b07400512d173958d1723d0a0239d01bb5dee5
josego85 <josego85@gmail.com>
2017-04-13 11:12:52 -0400

    Script para descargar LibreOffice 5.3.2 en Debian Jessie de 64 bits

---
+++ scripts/LibreOffice/instalarLibreOffice5_3_2__64bits.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Autor: josego
+# Blog: proyectosbeta.net
+# Fecha: 13 de abril de 2017 a las 10:27 hs.
+# Desinstala por completo si se tiene instalado LibreOffice.
+# Se instala LibreOffice 5.3.2 en español con su manual de ayuda correspondiente.
+apt-get purge libreoffice* -y
+apt-get remove libreoffice*
+apt-get autoremove -y
+
+# Creamos una carpeta nueva llamada libreoffice en /tmp 
+cd /tmp/
+mkdir libreoffice
+
+# Entramos a la carpeta que recien creamos.
+cd libreoffice
+
+# Descargamos los tres paquetes deb para tener instalado LibreOffice en español con su respectiva ayuda.
+wget -c http://download.documentfoundation.org/libreoffice/stable/5.3.2/deb/x86_64/LibreOffice_5.3.2_Linux_x86-64_deb.tar.gz
+wget -c http://download.documentfoundation.org/libreoffice/stable/5.3.2/deb/x86_64/LibreOffice_5.3.2_Linux_x86-64_deb_langpack_es.tar.gz
+wget -c http://download.documentfoundation.org/libreoffice/stable/5.3.2/deb/x86_64/LibreOffice_5.3.2_Linux_x86-64_deb_helppack_es.tar.gz
+
+# Descomprimimos.
+tar xzvf LibreOffice_5.3.2_Linux_x86-64_deb.tar.gz
+cd LibreOffice_5.3.2.2_Linux_x86-64_deb/DEBS/
+
+# Instalamos todos los paquetes deb de LibreOffice.
+dpkg -i *.deb
+
+# Descomprimimos.
+tar xzvf /tmp/libreoffice/LibreOffice_5.3.2_Linux_x86-64_deb_langpack_es.tar.gz
+cd LibreOffice_5.3.2.2_Linux_x86-64_deb_langpack_es/DEBS/
+
+# Instalamos todos los paquetes deb.
+dpkg -i *.deb
+
+# Descomprimimos.
+tar xzvf /tmp/libreoffice/LibreOffice_5.3.2_Linux_x86-64_deb_helppack_es.tar.gz
+cd LibreOffice_5.3.2.2_Linux_x86-64_deb_helppack_es/DEBS/
+
+# Instalamos todos los paquetes deb del idioma español.
+dpkg -i libobasis5.3-es-help_5.3.2.2-2_amd64.deb
+
+# Borramos la carpeta donde se encontraban los tres paquetes deb de LibreOffice.
+rm -R /tmp/libreoffice

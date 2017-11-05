95c245c565d6b843e842652cfb3bd22392385287
= <=>
2017-11-05 00:29:43 -0300

    Se agrega comentario al script ping.sh

--- scripts/GNU_Linux/bash/ping.sh
+++ scripts/GNU_Linux/bash/ping.sh
@@ -1,3 +1,8 @@
+###############################################
+#
+# Necesita los siguientes paquetes.
+# sudo apt install festival festvox-ellpc11k
+###############################################
 #!/bin/bash
 while :; do
     ping proyectosbeta.net -c 1 $1 && (echo "Ping" | festival --tts) || (echo "Fail" | festival --tts)
